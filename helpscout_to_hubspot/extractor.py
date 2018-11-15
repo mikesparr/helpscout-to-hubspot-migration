0ab8829766ba7eeadc21b7add7a015f4f5410e57
Mike Sparr <msparr@pointsixsolutions.com>
2018-11-15 16:12:32 -0700

    Progress on extractor with pagination

--- helpscout_to_hubspot/extractor.py
+++ helpscout_to_hubspot/extractor.py
@@ -1,4 +1,6 @@
 import os
+import time
+import urllib
 import logging
 import requests
 from envparse import env
@@ -6,24 +8,94 @@ from envparse import env
 env.read_envfile()
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
+KEYS = {
+    "Customer": "customers",
+    "Conversation": "conversations",
+    "Error": "error_description",
+    "Mailbox": "mailboxes"
+}
+
 def help():
     return (u"You need to get access keys from each API and update config first")
 
-def get_mailboxes():
-    url = env.str("HELPSCOUT_API_URL")
-    endpoint = "/mailboxes"
+def _get_page(url):
     token = env.str("HELPSCOUT_API_TOKEN")
     headers = {"Authorization": "Bearer {}".format(token)}
-
-    # fetch data
-    logging.info("Fetching data")
-    response = requests.get(url+endpoint, headers=headers)
+    logging.info("Fetching URL: {}".format(url))
+    response = requests.get(url, headers=headers)
     return response.json()
 
+def _get_next_page(obj):
+    links = obj["_links"]
+    next_page_url = links.get("next")["href"]
+
+    time.sleep(.075) # avoid exceeding 10 req/sec rate limit
+    return _get_page(next_page_url)
+
+def _has_error(obj):
+    return True if obj.get("error", None) is not None else False
+
+def _has_next_page(obj):
+    if _has_error(obj):
+        return False
+
+    links = obj["_links"]
+    return True if links.get("next", None) is not None else False
+
+def _get_initial_records(type, params = None):
+    path = env.str("HELPSCOUT_API_URL")
+    endpoint = "/{}".format(type)
+    url = (path + endpoint)
+
+    # add querystring params if exist (Dictionary)
+    if (params is not None):
+        url += "?{}".format(urllib.urlencode(params))
+
+    return _get_page(url)
+
+
+# records
+def get_records(type, params = None):
+    return _get_initial_records(type, params)
+
+def get_all_records(type, params = None):
+    response = get_records(type, params)
+    records = []
+
+    if _has_error(response):
+        logging.error("Error: {}".format(response[KEYS["Error"]]))
+    else:
+        # add initial page to list
+        records.extend(response["_embedded"][type])
+
+        # if multiple pages, loop, fetch and append additional records
+        while _has_next_page(response):
+            response = _get_next_page(response)
+            records.extend(response["_embedded"][type])
+    
+    logging.info("Returning {} {}".format(len(records), type))
+    return records
+
+def get_mailbox_ids():
+    response = get_records(KEYS["Mailbox"])
+    mailboxes = response["_embedded"][KEYS["Mailbox"]]
+    id_list = [box["id"] for box in mailboxes]
+    return id_list
+
+
 def main():
-    logging.info('Started')
-    get_mailboxes()
-    logging.info('Finished')
+    logging.info(KEYS["Mailbox"])
+    print(get_mailbox_ids)
+
+    logging.info(KEYS["Conversation"])
+    conversations = get_records(KEYS["Conversation"], {"status": "all"})
+    print(_has_next_page(conversations))
+
+    logging.info("All conversations")
+    _ = get_all_records(KEYS["Conversation"], {"status": "all"})
+
+    logging.info("Finished")
+
 
 if __name__ == '__main__':
     main()
--- helpscout_to_hubspot/tests/test_extractor.py
+++ helpscout_to_hubspot/tests/test_extractor.py
@@ -2,35 +2,43 @@ from unittest import TestCase
 
 import helpscout_to_hubspot as h2h
 
+KEYS = h2h.extractor.KEYS
+
 class TestHelp(TestCase):
     def test_is_string(self):
         s = h2h.help()
         self.assertTrue(isinstance(s, basestring))
 
 class TestMailboxes(TestCase):
-    # you must add an HELPSCOUT_API_TOKEN to .env before these will work (see README.md)
-    def test_get_mailboxes(self):
-        result = h2h.extractor.get_mailboxes()
-        mailboxes = result["_embedded"]["mailboxes"]
-        self.assertIsNotNone(mailboxes)
-        self.assertGreaterEqual(len(mailboxes), 3)
+    # you must add an HELPSCOUT_API_TOKEN to .env before these will work (see README.md)  
+    def test_get_mailbox_ids(self):
+        result = h2h.extractor.get_mailbox_ids()
+        self.assertGreater(len(result), 0)
 
 class TestCustomers(TestCase):
-    def test_fetch_data(self):
-        self.assertTrue(True)
-
-    def test_paginate(self):
-        self.assertTrue(True)
+    def test_get_records(self):
+        result = h2h.extractor.get_records(KEYS["Customer"])
+        customers = result["_embedded"][KEYS["Customer"]]
+        self.assertIsNotNone(customers)
+        self.assertGreater(len(customers), 5)
+    
+    def test_get_all_customers(self):
+        result = h2h.extractor.get_all_records(KEYS["Customer"])
+        self.assertGreater(len(result), 50) # multiple pages 
     
     def test_write_file(self):
         self.assertTrue(True)
 
 class TestConversations(TestCase):
-    def test_fetch_list(self):
-        self.assertTrue(True)
-
-    def test_paginate(self):
-        self.assertTrue(True)
+    def test_get_records(self):
+        result = h2h.extractor.get_records(KEYS["Conversation"], {"status": "all"})
+        conversations = result["_embedded"][KEYS["Conversation"]]
+        self.assertIsNotNone(conversations)
+        self.assertGreater(len(conversations), 5)
+    
+    def test_get_all_conversations(self):
+        result = h2h.extractor.get_all_records(KEYS["Conversation"], {"status": "all"})
+        self.assertGreater(len(result), 50) # multiple pages 
     
     def test_fetch_detail(self):
         self.assertTrue(True)
